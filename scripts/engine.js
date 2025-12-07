8c284d08e023a7ec235db041cece2264f1297271
DevDias <learn.now.e@gmail.com>
2025-12-06 23:20:05 -0300

    implement game rendering logic and add settings for code completion

--- scripts/engine.js
+++ scripts/engine.js
@@ -1,25 +1,44 @@
-var folhaDesenho = document.getElementById("folha");
-var areaDesenho = folhaDesenho.getContext("2d"); 
+window.onload = function() {
+    this.setInterval(executarJogo, 1000/30);
+};
 
-/* Configura a cor de fundo do campo */
-areaDesenho.fillStyle = "#286047";
-areaDesenho.fillRect(0, 0, folhaDesenho.width, folhaDesenho.height);
+var posicaoBolaX = posicaoBolaY = 10;
 
-/* Configura linha central do campo */
-areaDesenho.fillStyle = "#FFFFFF";
-areaDesenho. fillRect (297.5, 0, 5, 500);
+function executarJogo() {
+    var folhaDesenho = document.getElementById("folha");
+    var areaDesenho = folhaDesenho.getContext("2d"); 
+    var larguraCampo = 600;
+    var alturaCampo = 500; 
+    var espessuraRede = 5;
+    var alturaRaquete = 10;
+    var diametroBola = 10;
 
-/* Desenha o jogador 1 */
-areaDesenho.fillStyle = "#FFFFFF";
-areaDesenho.fillRect(10, 200, 10, 100);
+    /* Configura a cor de fundo do campo */
+    areaDesenho.fillStyle = "#286047";
+    areaDesenho.fillRect(0, 0, larguraCampo, alturaCampo);
 
-/* Desenha o jogador 2 */
-areaDesenho.fillStyle = "#FFFFFF";
-areaDesenho.fillRect(575, 200, 10, 100);
+    /* Configura a cor das linhas do campo */
+    areaDesenho.fillStyle = "#FFFFFF";
 
-/* Desenha a bola */   
-areaDesenho.beginPath();
-areaDesenho.arc(300, 250, 10, 0, 2 * Math.PI);
-areaDesenho.fill();
+    /* Configura linha central do campo */
+    areaDesenho. fillRect (larguraCampo/2 - espessuraRede/2, 0, espessuraRede, alturaCampo);
+
+    /* Jogador 1 */
+    areaDesenho.fillRect(10, 200, alturaRaquete, 100);
+
+    /* Jogador 2 */
+    areaDesenho.fillRect(580, 200, alturaRaquete, 100);
+
+    /* Desenha a bola */
+    areaDesenho.fillRect(posicaoBolaX - diametroBola/2, posicaoBolaY - diametroBola/2, diametroBola, diametroBola);   
+    //areaDesenho.beginPath();
+    //areaDesenho.arc(300, 250, 10, 0, 2 * Math.PI);
+    //areaDesenho.fill();
+
+    posicaoBolaX = posicaoBolaX + 1;
+    posicaoBolaY = posicaoBolaY + 1;
+};
+
+
 
 
